fe6abbe06f20227a477e4c81841dbdb3a2d39e4a
John Welch <132951168+jdw004@users.noreply.github.com>
2024-10-29 18:25:22 -0500

    Update project3_Welch_jdw0156.cpp
    final phase 1

--- Project-3/project3_Welch_jdw0156.cpp
+++ Project-3/project3_Welch_jdw0156.cpp
@@ -8,33 +8,58 @@
  
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
+#include <stdexcept>
 
 
 using namespace std;
 
 const int MAX_SIZE = 1000;
 
+// Check if file is Valid
+bool isValidFloat(const string& str) {
+    try {
+        size_t pos;
+        stof(str, &pos);
+        return pos == str.length(); 
+    } catch (const invalid_argument&) {
+        return false;
+    } catch (const out_of_range&) {
+        return false;
+    }
+}
+
+
 // Function to read file and store values in array
 int readFile(float userArray[], ifstream& instream) {
     int index = 0;
-    instream >> userArray[index];
-    while (!instream.eof()) {
-        index++;
-        instream >> userArray[index];
+    string line;
+
+    while (getline(instream, line)) {
+        if (isValidFloat(line)) {
+            userArray[index++] = stof(line);  
+        } else {
+            // If invalid return -1
+            return -1;
+        }
     }
-    return index + 1;  
+    return index;
 }
+
 
 // Function to write file
 void writeFile(float outputArray[], int outputArray_size, float mean, float median, float mode, const string& outputFilename) {
     ofstream outFile(outputFilename);
     if (outFile.is_open()) {
         outFile << "*** Summarized Statistics ***\n";
+        outFile << "\n";
         outFile << "The orderly sorted list of " << outputArray_size << " values is:\n";
         for (int i = 0; i < outputArray_size; ++i) {
             outFile << outputArray[i] << " ";
         }
+        outFile << "\n";
+
         outFile << "\nThe mean is " << mean << "\n";
         outFile << "The median is " << median << "\n";
         outFile << "The mode is " << mode << "\n";
@@ -164,14 +189,13 @@ float findMode(float sorted_array[], int size) {
 int main() {
     cout << endl << "*** Welcome to John's Data Analyzer ***\n";
     cout << "Enter the number of files to read: ";
-
     int numFiles;
     cin >> numFiles;
+    cout << endl;
+
     int count = 1;
     int totalNumCount = 0;
-    bool validFile;
     ifstream instream;
-
     int arraySizes[numFiles + 1];
     float array[numFiles + 1][MAX_SIZE];
     float tempArray[MAX_SIZE];
@@ -179,51 +203,75 @@ int main() {
     // Loop until all files are read
     while (numFiles > 0) {
         string filename;
-        cout << "Enter the filename for file " << count << ": ";
-        cin >> filename;
+        bool fileReadSuccessfully = false;
+        
+        while (!fileReadSuccessfully) {
+            cout << "Enter the filename for file " << count << ": ";
+            cin >> filename;
 
-        // Check if user wants to quit
-        if (filename == "quit") {
-            if (count == 1) {
-                cout << "Input cancelled. Proceeding to calculation..." << endl;
-                return 0;
+            if (filename == "quit") {
+                if (count == 1) {
+                    cout << "Input cancelled. Proceeding to calculation..." << endl;
+                    cout << endl;
+                    cout << "*** Goodbye. ***" << endl;
+                    return 0;
+                } 
+                else {
+                    cout << "Input cancelled. Proceeding to calculation..." << endl;
+                    break;
+                }
+            }
+
+            instream.open(filename.c_str());
+            if (instream.fail()) {
+                cout << "File not found. Please try again.\n";
+                instream.clear();
+                continue;
+            }
+
+            // Read values from file and check if valid
+            int numValidEntries = readFile(tempArray, instream);
+            instream.close();
+
+            if (numValidEntries == -1) {
+                cout << "Not an input file. Illegal content/structure detected. Please try again.\n";
+                cout << endl;
+                continue; 
             } else {
-                cout << "Input cancelled. Proceeding to calculation..." << endl;
-                break;
+                // File is valid
+                fileReadSuccessfully = true;
+                arraySizes[count] = numValidEntries;
+                totalNumCount += arraySizes[count];
+
+                cout << "The list of " << arraySizes[count] << " numbers in file " << filename << " is:\n";
+                for (int i = 0; i < arraySizes[count]; i++) {
+                    cout << tempArray[i] << "\n";
+                    array[count][i] = tempArray[i];
+                }
+                cout << endl;
             }
         }
 
-        instream.open(filename.c_str());
-        validFile = instream.good();
-
-        if (!validFile) {
-            cout << "Error: Invalid filename, please try again\n";
-            instream.clear();
-            instream.close();
-            continue;
-        }
-
-        // Read values from file
-        arraySizes[count] = readFile(tempArray, instream);
-        totalNumCount += arraySizes[count];
-        instream.close();
-
-        cout << "The list of " << arraySizes[count] << " numbers in file " << filename << " is:\n";
-        for (int i = 0; i < arraySizes[count]; i++) {
-            cout << tempArray[i] << "\n";
-        }
-
-        for (int i = 0; i < arraySizes[count]; i++) {
-            array[count][i] = tempArray[i];
-        }
-        cout << endl;
+        if (filename == "quit") break;
+        
         count++;
         numFiles--;
     }
 
-    // Find and display statistics
+    cout << endl;
+    cout << "*** Summarized Statistics ***" << endl;
+    cout << endl;
+    cout << "The orderly sorted list of " << totalNumCount << " values is: " << endl;
+
+    // Sort and print
     float sortedArray[totalNumCount];
     int size = sort2DArray(array, arraySizes, count - 1, sortedArray, totalNumCount);
+    for (int i = 0; i < size; i++) {
+        cout << sortedArray[i] << " ";
+    }
+    cout << endl;
+
+    // Find and display statistics
     float mean = findMean(sortedArray, size);
     float median = findMedian(sortedArray, size);
     float mode = findMode(sortedArray, size);
@@ -235,10 +283,15 @@ int main() {
 
     // Prompt for output filename and write to output file
     cout << "Enter the output filename: ";
+
     string outputFilename;
     cin >> outputFilename;
+    cout << endl;
+
     writeFile(sortedArray, size, mean, median, mode, outputFilename);
 
     cout << "*** Goodbye. ***" << endl;
     return 0;
 }
+
+
